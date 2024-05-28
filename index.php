617d900bf90c74ce640ca02aa385a162a125a676
Juan Dlc <91905741+JuanDlc10@users.noreply.github.com>
2024-05-28 15:40:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
+use config\Config;
+
 require_once realpath('./vendor/autoload.php');
+$config = new Config();
 ?>
 <!doctype html>
 <html lang="en">
@@ -10,16 +13,19 @@ require_once realpath('./vendor/autoload.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="./public/img/Hektakun-Pokemon-094-Gengar.72.png" type="image/x-icon">
     <title>Proyecto Final</title>
-    <link rel="stylesheet" href="./public/css/style.css">
-    <link rel="stylesheet" href="./public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./public/css/sweetalert.min.css">
-    <script src="./public/js/sweetalert.all.min.js"></script>
-    <script src="./controller/coquita.controller.js"></script>
+    <link rel="stylesheet" href="<?= $config->depCss('bootstrap.min.css') ?>">
+    </link>
+    <link rel="stylesheet" href="<?= $config->depCss('style.css') ?>">
+    </link>
+    <link rel="stylesheet" href="<?= $config->depCss('sweetalert.min.css') ?>">
+    </link>
+    <script src="<?= $config->depJs('sweetalert.all.min.js') ?>"></script>
+    <script src="<?= $config->depJs('coquita.controller.js') ?>"></script>
 </head>
 
 <body>
     <?php require_once './app/config/MyRoutes.php'; ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="<?= $config->depJs('bootstrap.bundle.min.js') ?>" crossorigin="anonymous"></script>
 </body>
 
 </html>
