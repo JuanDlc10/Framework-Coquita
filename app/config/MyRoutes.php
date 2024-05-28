41e2b9555813f7ae5dc51977d5d442063c9f3f88
Juan Dlc <daner_10@live.com.mx>
2024-05-27 21:09:33 -0600

    v1
    v1

--- app/config/MyRoutes.php
+++ app/config/MyRoutes.php
@@ -7,13 +7,8 @@ use config\Config;
 require_once realpath('./vendor/autoload.php');
 $router = new Config;
 
-$router->get("/", ['Login', 'index']);
-$router->get("/sigin", ['Login', 'sigin']);
-$router->post("/validarLogin", ['Login', 'verificar_login']);
-$router->post("/validarRegistro", ['Sigin', 'verificar_login']);
-$router->get("/logout", ['Login', 'cerrar_sesion']);
-$router->get("/inicio", ['Productos', 'index']);
-$router->get("/agregar", ['Productos', 'agregar_vista']);
+$router->get("/", ['Example', 'index']);
+
 
 $router->run();
 
